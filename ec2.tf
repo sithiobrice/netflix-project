81da99dc23c167480fea8eedfc47fdb2da9af8a5
sithiobrice <sithiobrice@gmail.com>
2022-08-10 21:23:43 -0400

    added new infra as for the requirementin task-18

--- ec2.tf
+++ ec2.tf
@@ -1,2 +1,6 @@
 # code to create ec2
 # https://www.youtube.com/watch?v=PzJCwfYfIzY
+# 1 git init to intialize the reposertry 
+# 2- git add to add an non satge file to the staging 
+# 3- git commit -m to commit (taking picture of the version )
+# 4- git log is to see the commit message that has happen
